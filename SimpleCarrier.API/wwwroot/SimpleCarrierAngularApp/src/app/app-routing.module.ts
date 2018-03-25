4c4744bf373467d978d3a5fd7ee09b194e4bd656
bogdanudovichenko <bogdanudovichenkodev@gmail.com>
2018-03-25 15:08:12 +0300

    change loadChildren from lambda to string because aot compilation dont support lambdas for loadChildren in router

--- SimpleCarrier.API/wwwroot/SimpleCarrierAngularApp/src/app/app-routing.module.ts
+++ SimpleCarrier.API/wwwroot/SimpleCarrierAngularApp/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
-import {AuthModule} from "./auth/auth.module";
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', redirectTo: '/auth/login', pathMatch: 'full' },
-  { path: 'auth', loadChildren: () => AuthModule }
+  { path: 'auth', loadChildren: './auth/auth.module#AuthModule' }
 ];
 
 @NgModule({
